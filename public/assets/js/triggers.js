68d3ec7572db2c06ed2d351061f6501ead728ee1
Felix Müllner <felix.muellner.96@gmail.com>
2017-08-23 01:10:47 +0200

    Add error message if website can't load triggers
    Also add comments for all the other client-side validation that
    still needs to be done.

--- public/assets/js/triggers.js
+++ public/assets/js/triggers.js
@@ -14,7 +14,7 @@ $(function () {
 		$.ajax({
 			contentType: 'application/json',
 			data: JSON.stringify(updatedTrigger),
-			error: $.noop(),
+			error: $.noop(), // TODO add error handling
 			method: 'post',
 			success: getAllTriggers,
 			url: '/api/trigger/update',
@@ -31,7 +31,7 @@ $(function () {
 		$.ajax({
 			contentType: 'application/json',
 			data: JSON.stringify(deletedTrigger),
-			error: $.noop(),
+			error: $.noop(), // TODO add error handling
 			method: 'post',
 			success: getAllTriggers,
 			url: '/api/trigger/delete',
@@ -52,7 +52,7 @@ $(function () {
 		$.ajax({
 			contentType: 'application/json',
 			data: JSON.stringify(newTrigger),
-			error: $.noop(),
+			error: $.noop(), // TODO add error handling
 			method: 'post',
 			success: getAllTriggers,
 			url: '/api/trigger/add',
@@ -73,7 +73,49 @@ function getAllTriggers() {
 }
 
 function getAllTriggersError(jqHXR, textStatus, errorThrown) {
-	// TODO
+	// delete all current rows from table
+	$('tbody').children().remove();
+
+	// display error message
+	let $triggerRow = $([
+		'<tr class="center aligned">',
+		'	<td colspan="6">',
+		'		<div class="ui error message">',
+		'			<p>An error occured while getting current triggers</p>',
+		'		</div>',
+		'	</td>',
+		'</tr>',
+	].join('\n'));
+
+	$('tbody').append($triggerRow);
+
+	// add last row to create new triggers to table
+	var $addTriggerRow = $([
+		'<tr class="center aligned">',
+		// TODO input validation
+		'	<td><div class="ui input"><input type="text" name="guildId"></div></td>',
+		'	<td><div class="ui input"><input type="text" name="channelId"></div></td>',
+		'	<td><div class="ui input"><input type="text" name="userId"></div></td>',
+		'	<td>',
+		'		<div class="ui selection dropdown">',
+		'			<input type="hidden" name="method">',
+		'			<i class="dropdown icon"></i>',
+		'			<div class="default text">Method</div>',
+		'			<div class="menu">',
+		'				<div class="item" data-value="exactly">Is Exactly</div>',
+		'				<div class="item" data-value="contains">Contains</div>',
+		'				<div class="item" data-value="regex">Matches Regex</div>',
+		'			</div>',
+		'		</div>',
+		'	</td>',
+		'	<td><div class="ui input"><input type="text" name="text"></div></td>',
+		'	<td><button class="ui fluid blue button">Add</button></td>',
+		'</tr>',
+	].join('\n'));
+
+	$('tbody').append($addTriggerRow);
+
+	$('.ui.dropdown').dropdown();
 }
 
 function getAllTriggersSuccess(data, textStatus, jqXHR) {
@@ -84,9 +126,11 @@ function getAllTriggersSuccess(data, textStatus, jqXHR) {
 	for (let trigger of data) {
 		let $triggerRow = $([
 			'<tr class="center aligned" data-id="' + trigger._id + '">',
-			'	<td><div class="ui input"><input type="text" name="guildId" value="' + trigger.guildId + '"></div><!--<br>TODO--></td>',
-			'	<td><div class="ui input"><input type="text" name="channelId" value="' + trigger.channelId + '"></div><!--<br>TODO--></td>',
-			'	<td><div class="ui input"><input type="text" name="userId" value="' + trigger.userId + '"></div><!--<br>TODO--></td>',
+			// TODO input validation
+			// TODO show names of current guild, channel and/or user
+			'	<td><div class="ui input"><input type="text" name="guildId" value="' + trigger.guildId + '"></div></td>',
+			'	<td><div class="ui input"><input type="text" name="channelId" value="' + trigger.channelId + '"></div></td>',
+			'	<td><div class="ui input"><input type="text" name="userId" value="' + trigger.userId + '"></div></td>',
 			'	<td>',
 			'		<div class="ui selection dropdown">',
 			'			<input type="hidden" name="method" value="' + trigger.method + '">',
@@ -115,6 +159,7 @@ function getAllTriggersSuccess(data, textStatus, jqXHR) {
 	// add last row to create new triggers to table
 	var $addTriggerRow = $([
 		'<tr class="center aligned">',
+		// TODO input validation
 		'	<td><div class="ui input"><input type="text" name="guildId"></div></td>',
 		'	<td><div class="ui input"><input type="text" name="channelId"></div></td>',
 		'	<td><div class="ui input"><input type="text" name="userId"></div></td>',
